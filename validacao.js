5e6340527e6109db3b28468347e074e91ef4bf81
VitorHugoTrajano <110561757+VitorHugoTrajano@users.noreply.github.com>
2022-12-27 12:55:58 -0300

    Projeto concluído

---
+++ validacao.js
@@ -0,0 +1,46 @@
+function verificaChute(chute) {
+    const numero = +chute
+
+    if(chuteInvalido(numero)) {
+        elementoChute.innerHTML += `<div>Valor inválido</div>`
+        return
+    }
+
+    if(numeroPermitido(numero)) {
+        elementoChute.innerHTML += `<div>Valor inválido: o número secreto
+        está entre ${menorValor} e ${maiorValor}</div>`
+        return
+    }
+
+    if(numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era: ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-sharp fa-solid fa-arrow-down-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-sharp fa-solid fa-arrow-up-long"></i></div>
+        `
+    }
+
+}
+
+function chuteInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroPermitido(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
